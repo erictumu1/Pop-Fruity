180b3803e619c9e10f18c787dcec714620fb7699
erictumu1 <erictumu@outlook.com>
2025-08-28 16:12:42 -0400

    Updates to the Carousel Slice

--- src/slices/Carousel/index.tsx
+++ src/slices/Carousel/index.tsx
@@ -37,19 +37,18 @@ export type CarouselProps = SliceComponentProps<Content.CarouselSlice>;
  * Component for "Carousel" Slices.
  */
 const Carousel: FC<CarouselProps> = ({ slice }) => {
-  const [currentFlavorIndex, setCurrentFlavourIndex] = useState(0);
+  const [currentFlavorIndex, setCurrentFlavorIndex] = useState(0);
+  const sodaCanRef = useRef<Group>(null);
 
-  const sodacanRef = useRef<Group>(null);
-
-  function changeFlavour(index: number) {
-    if (!sodacanRef.current) return;
+  function changeFlavor(index: number) {
+    if (!sodaCanRef.current) return;
 
     const nextIndex = (index + FLAVORS.length) % FLAVORS.length;
 
     const tl = gsap.timeline();
 
     tl.to(
-      sodacanRef.current.rotation,
+      sodaCanRef.current.rotation,
       {
         y:
           index > currentFlavorIndex
@@ -61,9 +60,9 @@ const Carousel: FC<CarouselProps> = ({ slice }) => {
       0,
     )
       .to(
-        ".background, .wavy-cirlces-outer, .wavy-circles-inner",
+        ".background, .wavy-circles-outer, .wavy-circles-inner",
         {
-          background: FLAVORS[nextIndex].color,
+          backgroundColor: FLAVORS[nextIndex].color,
           fill: FLAVORS[nextIndex].color,
           ease: "power2.inOut",
           duration: 1,
@@ -71,69 +70,63 @@ const Carousel: FC<CarouselProps> = ({ slice }) => {
         0,
       )
       .to(".text-wrapper", { duration: 0.2, y: -10, opacity: 0 }, 0)
-      .to({}, { onStart: () => setCurrentFlavourIndex(nextIndex) }, 0.5)
-      .to(".text-wrapper", { duration: 2, y: 0, opacity: 1 }, 0.7);
-
-    setCurrentFlavourIndex(nextIndex);
+      .to({}, { onStart: () => setCurrentFlavorIndex(nextIndex) }, 0.5)
+      .to(".text-wrapper", { duration: 0.2, y: 0, opacity: 1 }, 0.7);
   }
 
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="carousel justify center relative grid h-screen grid-rows-[auto,4fr,auto] overflow-hidden bg-white py-12 text-white"
+      className="carousel relative grid h-screen grid-rows-[auto,4fr,auto] justify-center overflow-hidden bg-white py-12 text-white"
     >
-      <div className="background absolute inset-0 bg-[#710523]/50">
-        <WavyCircles className="absolute top-1/2 left-1/2 z-0 h-[120vmin] -translate-x-1/2 -translate-y-1/2 text-[#710523] opacity-50" />
+      <div className="background pointer-events-none absolute inset-0 bg-[#710523] opacity-50" />
 
-        <h2 className="relative text-center text-5xl font-bold">
-          <PrismicText field={slice.primary.heading} />
-        </h2>
+      <WavyCircles className="absolute top-1/2 left-1/2 h-[120vmin] -translate-x-1/2 -translate-y-1/2 text-[#710523]" />
 
-        <div className="relative z-10 flex items-center justify-center gap-4">
-          {/* Left Button */}
-          <ArrowButton
-            onClick={() => changeFlavour(currentFlavorIndex - 1)}
-            direction="left"
-            label="Previous Flavor"
+      <h2 className="relative text-center text-5xl font-bold">
+        <PrismicText field={slice.primary.heading} />
+      </h2>
+
+      <div className="flex items-center justify-center space-x-4">
+        {/* Left */}
+        <ArrowButton
+          onClick={() => changeFlavor(currentFlavorIndex + 1)}
+          direction="left"
+          label="Previous Flavor"
+        />
+        {/* Can */}
+        <View className="aspect-square h-[70vmin] min-h-40">
+          <Center position={[0, 0, 1.5]}>
+            <FloatingCan
+              ref={sodaCanRef}
+              floatIntensity={0.3}
+              rotationIntensity={1}
+              flavor={FLAVORS[currentFlavorIndex].flavor}
+            />
+          </Center>
+
+          <Environment
+            files="/hdr/lobby.hdr"
+            environmentIntensity={0.6}
+            environmentRotation={[0, 3, 0]}
           />
+          <directionalLight intensity={6} position={[0, 1, 1]} />
+        </View>
+        {/* Right */}
+        <ArrowButton
+          onClick={() => changeFlavor(currentFlavorIndex - 1)}
+          direction="right"
+          label="Next Flavor"
+        />
+      </div>
 
-          {/* Can */}
-          <div className="aspect-square h-[70vmin] min-h-40 w-[70vmin]">
-            <View className="h-full w-full">
-              <Center position={[0, 0, 1.5]}>
-                <FloatingCan
-                  floatIntensity={0.3}
-                  rotationIntensity={1}
-                  flavor={FLAVORS[currentFlavorIndex].flavor}
-                  ref={sodacanRef}
-                />
-              </Center>
-              <Environment
-                files="/hdr/lobby.hdr"
-                environmentIntensity={0.6}
-                environmentRotation={[0, 3, 0]}
-              />
-              <directionalLight intensity={6} position={[0, 1, 1]} />
-            </View>
-          </div>
-
-          {/* Right Button */}
-          <ArrowButton
-            onClick={() => changeFlavour(currentFlavorIndex + 1)}
-            direction="right"
-            label="Next Flavor"
-          />
+      <div className="text-area relative mx-auto text-center">
+        <div className="text-wrapper text-4xl font-medium">
+          <p>{FLAVORS[currentFlavorIndex].name}</p>
         </div>
-
-        <div className="text-area relative mx-auto text-center">
-          <div className="text-wrapper text-4xl font-medium">
-            <p>{FLAVORS[currentFlavorIndex].name}</p>
-          </div>
-
-          <div className="mt-2 text-2xl font-normal opacity-90">
-            <PrismicText field={slice.primary.price_copy} />
-          </div>
+        <div className="mt-2 text-2xl font-normal opacity-90">
+          <PrismicText field={slice.primary.price_copy} />
         </div>
       </div>
     </section>
