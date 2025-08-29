70e32c4088465581b195a78eecb8615d5a2f696b
erictumu1 <erictumu@outlook.com>
2025-08-29 17:46:51 -0400

    .

--- src/slices/Carousel/index.tsx
+++ src/slices/Carousel/index.tsx
@@ -179,7 +179,7 @@ const Carousel: FC<CarouselProps> = ({ slice }) => {
 
       {/* Footer */}
       <footer
-        className="absolute bottom-0 w-full py-4 text-center transition-colors duration-500"
+        className="absolute bottom-0 w-full py-8 text-center transition-colors duration-500"
         style={{
           backgroundColor: FLAVORS[currentIndex].color, // Footer background color
         }}
