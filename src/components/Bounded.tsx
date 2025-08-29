9424ce147ce2f79459f5ca33cb088f9b0cdea21f
erictumu1 <erictumu@outlook.com>
2025-08-29 17:02:07 -0400

    .

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,17 +1,18 @@
 import clsx from "clsx";
+import React from "react";
 
-type BoundedProps = {
-  as?: React.ElementType;
+type BoundedProps<T extends React.ElementType = "section"> = {
+  as?: T;
   className?: string;
-  children: React.ReactNode;
-};
+  children: React.ReactNode; // This can be any renderable content
+} & React.ComponentProps<T>; // Directly use ComponentProps<T> to avoid omitting refs
 
-export const Bounded = ({
-  as: Comp = "section",
+export const Bounded = <T extends React.ElementType = "section">({
+  as: Comp = "section" as T,
   className,
   children,
   ...restProps
-}: BoundedProps) => {
+}: BoundedProps<T>) => {
   return (
     <Comp
       className={clsx("px-4 first:pt-10 md:px-6", className)}
